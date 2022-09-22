647ceae058af186b0e9d85199990bca42e5bf30c
bracen <ninashindesign@gmail.com>
2022-09-22 16:22:21 +1200

    added function for max and min temp convert to celsius to Fahrenheit v.v

--- src/index.js
+++ src/index.js
@@ -28,7 +28,7 @@ function dateFormat(timestamp) {
   if (minutes < 10) {
     minutes = `0${minutes}`;
   }
-  let ampm = hours >= 12 ? 'pm' : 'am';
+  let ampm = hours >= 12 ? "pm" : "am";
   hours = hours % 12;
   hours = hours ? hours : 12;
 
@@ -42,15 +42,16 @@ function showWeatherCondition(response) {
   let dateElement = document.querySelector("#date");
   let minDayTemp = document.querySelector("#dayTempMin");
   let maxDayTemp = document.querySelector("#dayTempMax");
-  
-  dateElement.innerHTML = dateFormat(response.data.dt*1000);
+  cityElement.innerHTML = response.data.name;
+  dateElement.innerHTML = dateFormat(response.data.dt * 1000);
+  //temperature
   maxDayTemp.innerHTML = Math.round(response.data.main.temp_max);
   minDayTemp.innerHTML = Math.round(response.data.main.temp_min);
-  cityElement.innerHTML = response.data.name;
-  
   temperatureElement.innerHTML = Math.round(response.data.main.temp);
+  minDayCelsius = response.data.main.temp_min;
+  maxDayCelsius = response.data.main.temp_max;
   celsiusTemperature = response.data.main.temp;
-  
+
   document.querySelector(
     "#description"
   ).innerHTML = `${response.data.weather[0].description}`;
@@ -67,7 +68,7 @@ function showWeatherCondition(response) {
     `http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`
   );
 }
-//functions
+// search functions
 function findCity(city) {
   let key = "5f472b7acba333cd8a035ea85a0d4d4c";
   let units = "metric";
@@ -79,7 +80,7 @@ function search(event) {
   let city = document.querySelector("#search-input");
   findCity(city.value);
 }
-
+//Fahrenheit Temp function
 function displayFahrenheitTemperature(event) {
   event.preventDefault();
   celsiusLink.classList.remove("active");
@@ -88,6 +89,22 @@ function displayFahrenheitTemperature(event) {
   let fahrenheitTemperature = (celsiusTemperature * 9) / 5 + 32;
   temperatureElement.innerHTML = Math.round(fahrenheitTemperature);
 }
+
+function displayLowFahrenheitTemperature(event) {
+  event.preventDefault();
+  let lowTemperatureElement = document.querySelector("#dayTempMin");
+  let minFahrenheitTemperature = (minDayCelsius * 9) / 5 + 32;
+  lowTemperatureElement.innerHTML = Math.round(minFahrenheitTemperature);
+}
+
+function displayMaxFahrenheitTemperature(event) {
+  event.preventDefault();
+  let highTemperatureElement = document.querySelector("#dayTempMax");
+  let maxFahrenheitTemperature = (maxDayCelsius * 9) / 5 + 32;
+  highTemperatureElement.innerHTML = Math.round(maxFahrenheitTemperature);
+}
+
+//Celsius Temp elememts
 function displayCelsiusTemperature(event) {
   event.preventDefault();
   celsiusLink.classList.add("active");
@@ -95,6 +112,19 @@ function displayCelsiusTemperature(event) {
   let temperatureElement = document.querySelector("#temperature");
   temperatureElement.innerHTML = Math.round(celsiusTemperature);
 }
+
+function displayLowCelsiusTemperature(event) {
+  event.preventDefault();
+  let lowTemperatureElement = document.querySelector("#dayTempMin");
+  lowTemperatureElement.innerHTML = Math.round(minDayCelsius);
+}
+
+function displayMaxCelsiusTemperature(event) {
+  event.preventDefault();
+  let highTemperatureElement = document.querySelector("#dayTempMax");
+  highTemperatureElement.innerHTML = Math.round(maxDayCelsius);
+}
+
 //let celsiusTemperature = null;
 
 let typeCity = document.querySelector("#search-form");
@@ -102,8 +132,12 @@ typeCity.addEventListener("submit", search);
 
 let fahrenheitLink = document.querySelector("#fahrenheit-link");
 fahrenheitLink.addEventListener("click", displayFahrenheitTemperature);
+fahrenheitLink.addEventListener("click", displayLowFahrenheitTemperature);
+fahrenheitLink.addEventListener("click", displayMaxFahrenheitTemperature);
 
 let celsiusLink = document.querySelector("#celsius-link");
 celsiusLink.addEventListener("click", displayCelsiusTemperature);
+celsiusLink.addEventListener("click", displayLowCelsiusTemperature);
+celsiusLink.addEventListener("click", displayMaxCelsiusTemperature);
 
 findCity("New York");
